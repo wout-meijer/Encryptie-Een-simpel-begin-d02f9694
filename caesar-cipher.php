67d0031250fc7f4eaedb04abd052da12b05e058e
= <=>
2020-05-12 11:29:10 +0200

    caesar cipher

---
+++ caesar-cipher.php
@@ -0,0 +1,27 @@
+<?php
+
+if (empty($argv[1])) {
+    print "no string given" . PHP_EOL;
+    exit();
+}
+
+$plain = $argv[1];
+$encoded = '';
+
+for ($i=0;$i<strlen($plain);$i++) {
+    $ascii = ord($plain[$i]);
+    if($ascii == 90) {
+        $ascii = 65;
+    } else if($ascii == 122) {
+        $ascii = 97;
+    }
+    else {
+        $ascii++;
+    }
+    $encoded[$i] = chr($ascii);
+}
+
+
+
+print "Plain: $plain" . PHP_EOL;
+print "Cipher: $encoded" . PHP_EOL;
